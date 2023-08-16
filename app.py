1a3d0e8662b74ca2d91637792eafa877403dab78
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2023-08-16 15:42:29 -0300

    feat: exibe o menu de operações
     - exibe o menu de operações e pergunta ao usuário qual operação ele deseja fazer.

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ __license__ = "unlicense"
 #entrada de dados
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
-
+print("Operações:\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão")
+operation = int(input("Digite o número da operação: "))
