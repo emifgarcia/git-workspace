c316d7f63c31ce46eefc35f615b6d0da1a232766
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2023-08-16 15:27:57 -0300

    feat: armazena duas variáveis de entrada
     - n1 é o primeiro número do tipo float a ser digitado pelo usuário
     - n2 é o segundo número do tipo float a ser digitado pelo usuário

---
+++ app.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+__version__ = "0.1.0"
+__author__ = "Marcelo Garcia"
+__license__ = "unlicense"
+
+#entrada de dados
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
