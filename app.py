184a241d337f230b6020100185d8f6f564333c0e
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2023-08-16 15:51:48 -0300

    feat: executa a operação com base na escolha do usuário

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Marcelo Garcia"
 __license__ = "unlicense"
 
@@ -9,3 +9,13 @@ n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 print("Operações:\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão")
 operation = int(input("Digite o número da operação: "))
+
+if operation == 1:
+    res = n1 + n2
+elif operation == 2:
+    res = n1 - n2
+elif operation == 3:
+    res = n1 * n2
+elif operation == 4:
+    res = n1 / n2
+print(f"O resultado da operação é: {res}")
