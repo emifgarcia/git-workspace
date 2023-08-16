38f575b0e83d8e236b5cbce82a96abc437f1e0ed
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2023-08-16 16:24:21 -0300

    feat: trata erro no menu de operações
     - pede a operação correta caso o usuário digite uma operação inexistente
     - resolve a issues Closes #1

--- app.py
+++ app.py
@@ -8,14 +8,22 @@ __license__ = "unlicense"
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 print("Operações:\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão")
-operation = int(input("Digite o número da operação: "))
 
-if operation == 1:
-    res = n1 + n2
-elif operation == 2:
-    res = n1 - n2
-elif operation == 3:
-    res = n1 * n2
-elif operation == 4:
-    res = n1 / n2
+flag = 1
+while flag:
+    operation = int(input("Digite o número da operação: "))
+    if operation == 1:
+        res = n1 + n2
+    elif operation == 2:
+        res = n1 - n2
+    elif operation == 3:
+        res = n1 * n2
+    elif operation == 4:
+        res = n1 / n2
+    else:
+        print("Opção inválida!")
+        continue
+    flag = 0
+
 print(f"O resultado da operação é: {res}")
+
